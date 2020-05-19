ceb88ddcc9e15d228c8558e84635ce5b42717710
Atharvahajare <64681247+Atharvahajare@users.noreply.github.com>
2020-05-19 12:35:36 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ function setup() {
   createCanvas(1200,800);
   fixedRect = createSprite(400, 100, 50, 80);
   fixedRect.shapeColor = "green";
-  //fixedRect.debug = true;
+ // fixedRect.debug = true;
   movingRect = createSprite(400, 800,80,30);
   movingRect.shapeColor = "green";
  // movingRect.debug = true;
@@ -14,17 +14,42 @@ function setup() {
 }
 
 function draw() {
-  background(0,0,0);  
-
-  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
-      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
-    movingRect.velocityX = movingRect.velocityX * (-1);
-    fixedRect.velocityX = fixedRect.velocityX * (-1);
-  }
-  if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
-    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
-    movingRect.velocityY = movingRect.velocityY * (-1);
-    fixedRect.velocityY = fixedRect.velocityY * (-1);
-  }
+  background(0,0,0);
+  
+  bounceoff(movingRect,fixedRect);
+if (isTouching()){
+  fixedRect.shapeColor = "blue"
+  movingRect.shapeColor = "blue"
+}
+else{
+  fixedRect.shapeColor = "green"
+  movingRect.shapeColor = "green"
+}
+  
   drawSprites();
+}
+function isTouching(){
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+    && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2  
+    && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+      && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
+        return true;
+        
+      }
+      else{
+        return false;
+        
+      }
+}
+function  bounceoff(object1,object2){
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+    && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
+  movingRect.velocityX = movingRect.velocityX * (-1);
+  fixedRect.velocityX = fixedRect.velocityX * (-1);
+}
+if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+  && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+  movingRect.velocityY = movingRect.velocityY * (-1);
+  fixedRect.velocityY = fixedRect.velocityY * (-1);
+}
 }
